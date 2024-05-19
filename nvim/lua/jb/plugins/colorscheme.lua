273312073789796c40bd3d8064b34bc4079d7faa
JB <jdbegley3@gmail.com>
2024-05-19 09:38:01 -0400

    mason and other items

--- nvim/lua/jb/plugins/colorscheme.lua
+++ nvim/lua/jb/plugins/colorscheme.lua
@@ -1,10 +1,18 @@
 return  { 
   --"folke/tokyonight.nvim",
-  "olimorris/onedarkpro.nvim",
+  --  "olimorris/onedarkpro.nvim",
+  "catppuccin/nvim",
+  name = "catppuccin",
   priority = 1000,
   config = function()
-    require("onedarkpro").setup()
-    vim.cmd("colorscheme onedark")
+    require("catppuccin").setup({
+      flavour = "frappe",
+      integrations = {
+        noice = true,
+        which_key = true,
+      }
+    })
+    vim.cmd.colorscheme "catppuccin"
   end
 }
 
--- nvim/lua/jb/plugins/dashboard.lua
+++ nvim/lua/jb/plugins/dashboard.lua
@@ -2,29 +2,7 @@ return {
   'nvimdev/dashboard-nvim',
   event = 'VimEnter',
   config = function()
-    require('dashboard').setup {
-      theme = 'doom',
-      config = {
-        header = {
-	[[                                                     ]],
-	[[  __  __                 __  __                      ]],
-	[[ /\ \/\ \               /\ \/\ \  __                 ]],
-	[[ \ \ `\\ \     __    ___\ \ \ \ \/\_\    ___ ___     ]],
-	[[  \ \ , ` \  /'__`\ / __`\ \ \ \ \/\ \ /' __` __`\   ]],
-	[[   \ \ \`\ \/\  __//\ \L\ \ \ \_/ \ \ \/\ \/\ \/\ \  ]],
-	[[    \ \_\ \_\ \____\ \____/\ `\___/\ \_\ \_\ \_\ \_\ ]],
-	[[     \/_/\/_/\/____/\/___/  `\/__/  \/_/\/_/\/_/\/_/ ]],
-	[[                                                     ]],
-  [[                  Hello, Friend                      ]],
-  [[]],
-  [[]],
-  [[]],
-        },
-        center = {
-          { action =  "Telescope find_files", desc = "Telescope Find Files", key = "f" },
-        },
-      },
-    }
+    require('dashboard').setup()
     end,
   dependencies = { {'nvim-tree/nvim-web-devicons'}}
 }
